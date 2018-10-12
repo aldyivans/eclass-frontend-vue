7de9c5a7ebab88947ed73eb9356426b26df32a07
aldy ivan <ivanbrutalart@gmail.com>
2018-10-12 22:48:47 +0700

    eclass frontend template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <HelloWorld/>
+    <Blue/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Blue from '@/components/Blue.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Blue
   }
 }
 </script>
