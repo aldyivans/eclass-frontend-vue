70f720da16123013d8dcad91b9ee670074577696
Dicky Fajar D <kyodix@gmail.com>
2018-10-17 21:31:28 +0700

    nambah  login google

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="login">
     <Blue/>
+    <div class="g-signin2" data-onsuccess="onSignIn"></div>
   </div>
 </template>
 
@@ -12,6 +13,16 @@ export default {
   name: 'login',
   components: {
     Blue
+  },
+  methods: {
+    onSignIn(googleUser) {
+		  var profile = googleUser.getBasicProfile();
+		  console.log('TOKEN: ' + googleUser.getAuthResponse().id_token);
+		  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+		  console.log('Name: ' + profile.getName());
+		  console.log('Image URL: ' + profile.getImageUrl());
+		  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+		}
   }
 }
 </script>
