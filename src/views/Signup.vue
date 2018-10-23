8a9d51814d483787f70e84bbe0fc0afee7d81ade
edikurniawan <edikurniawanwibowo@gmail.com>
2018-10-23 12:33:22 +0700

    signup

---
+++ src/views/Signup.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="Signup">
+    <Blue/>
+    <SignupComponent/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Blue from '@/components/Blue.vue'
+import SignupComponent from '@/components/Signup/Signup.vue'
+
+export default {
+  name: 'Signup',
+  components: {
+    Blue,
+    SignupComponent
+  }
+}
+</script>
