f4550db783673f538648455bfd9438142ebdcfa3
aldy ivan <ivanbrutalart@gmail.com>
2018-10-23 12:49:14 +0700

    profile

---
+++ src/views/Profile.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="profile">
+    <Blue/>
+    <Profile/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Blue from '@/components/Blue.vue'
+import Profile from '@/components/Profile/Profile.vue'
+
+export default {
+  name: 'profile',
+  components: {
+    Blue,
+    Profile
+  }
+}
+</script>
