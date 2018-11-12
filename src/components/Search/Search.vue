be6f8f4c82a1c21574c079af3c2c67da6c95419f
edikurniawan <edikurniawanwibowo@gmail.com>
2018-11-12 10:45:01 +0700

    seacrh

---
+++ src/components/Search/Search.vue
@@ -0,0 +1,62 @@
+<template>
+	<div class="search bg-light">
+		<div class="container p-0">
+      <div class="row m-0 py-5">
+        <div class=" col-12 p-3">
+            <h5 class="m-0 font-weight-bold">XX Result For JavaScript</h5>
+        </div>
+        <div class="col-12 col-lg-8 p-2">
+          <div class="media border p-2 d-inline-block d-md-flex bg-white">
+            <div class="col-12 col-md-3 col-lg-3 p-0">
+              <img class="m-0 w-100" src="../../assets/js.jpg">
+            </div>
+            <div class="media-body mb-2 mb-md-0 ml-md-2">
+              <p class="m-0 pb-3 pb-lg-0">
+              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+              <div class="align-self-end text-right">
+                <font-awesome-icon icon="eye" /><p class="d-inline-block m-0 ml-1">24 views</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-lg-8 p-2">
+          <div class="media border p-2 d-inline-block d-md-flex bg-white">
+            <div class="col-12 col-md-3 col-lg-3 p-0">
+              <img class="m-0 w-100" src="../../assets/js.jpg">
+            </div>
+            <div class="media-body mb-2 mb-md-0 ml-md-2">
+              <p class="m-0 pb-3 pb-lg-0">
+              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+              <div class="align-self-end text-right">
+                <font-awesome-icon icon="eye" /><p class="d-inline-block m-0 ml-1">24 views</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex justify-content-center col-lg-12 px-2 m-0">
+          <nav aria-label="Page navigation example">
+            <ul class="pagination py-5 m-0">
+              <li class="page-item">
+                <a class="page-link" href="#" aria-label="Previous">
+                  <span aria-hidden="true">&laquo;</span>
+                  <span class="sr-only">Previous</span>
+                </a>
+              </li>
+              <li class="page-item"><a class="page-link" href="#">1</a></li>
+              <li class="page-item"><a class="page-link" href="#">2</a></li>
+              <li class="page-item"><a class="page-link" href="#">3</a></li>
+              <li class="page-item"><a class="page-link" href="#">4</a></li>
+              <li class="page-item"><a class="page-link" href="#">5</a></li>
+              <li class="page-item">
+                <a class="page-link" href="#" aria-label="Next">
+                  <span aria-hidden="true">&raquo;</span>
+                  <span class="sr-only">Next</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+	</div>
+</template>
